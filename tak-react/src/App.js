0570ebd15539599251785f44f08a474dd0806891
takgung6820 <takgung6820@gmail.com>
2022-06-05 17:04:58 +0900

    퇴사짤 대사 구현

--- tak-react/src/App.js
+++ tak-react/src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import Hello from './Hello';
 
 function App() {
+  const name = 'Wontak';
   return (
-    <div>
+    <>
       <Hello />
-      <Hello />
-      <Hello />
-    </div>
-  )
+      <div>안녕히 계세요 여러분! <br /> 
+           저 {name}은 이 세상의 모든 굴레와 속박을 벗어 던지고 제 행복을 찾아 떠납니다! <br />
+           여러분도 행복하세요~~!</div>
+    </>
+  );
 }
 
 export default App;
