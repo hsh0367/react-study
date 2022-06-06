3e17f73ea531c8234ed7ca37e50dd992b3c6290d
takgung6820 <takgung6820@gmail.com>
2022-06-06 14:22:51 +0900

    useState로 카운터 표현하기

--- tak-react/src/App.js
+++ tak-react/src/App.js
@@ -1,13 +1,9 @@
 import React from 'react';
-import Hello from './Hello';
-import Wrapper from './Wrapper';
+import Counter from './Counter';
 
 function App() {
   return (
-    <Wrapper>
-      <Hello name = "Wontak Lee" color = "blue" isSpecial/>
-      <Hello color = "magenta"/>
-    </Wrapper>
+    <Counter />
   );
 }
 
