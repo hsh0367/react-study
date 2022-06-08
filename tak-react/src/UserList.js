6e3b6c56e99343eeaa1eba88923201b99c977382
takgung6820 <takgung6820@gmail.com>
2022-06-08 21:05:34 +0900

    useRef로 컴포넌트 내 변수 생성

--- tak-react/src/UserList.js
+++ tak-react/src/UserList.js
@@ -8,28 +8,11 @@ function User({ user }) {
   );
 }
 
-function UserList() {
-  const users = [
-    {
-      id: 1,
-      username: 'Wontak Lee',
-      email: 'takgung6820@gmail.com'
-    }, 
-    {
-      id: 2,
-      username: 'Company Tak',
-      email: 'wontak.lee@gwebscorp.com'
-    }, 
-    {
-      id: 3,
-      username: 'Bu-kae Tak',
-      email: 'roundt2505@naver.com'
-    }
-  ];
+function UserList({ users }) {
   return (
     <div>
       {users.map(user => (
-        <User user={user} />
+        <User user={user} key={user.id} />
       ))}
     </div>
   );
