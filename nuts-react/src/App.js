662b94553679cecc2c8e114dc2c1851423c1c97a
hsh0367 <hsh0367@gmail.com>
2022-06-10 02:29:15 +0900

    feat: ch7 공부 추가 밸로퍼트 react 사이트

--- nuts-react/src/App.js
+++ nuts-react/src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import Hello from './hello';
 import './App.css';
-import Wrapper from './Wrapper';
 import Counter from './Counter';
 function App() {
   const name = "first";
@@ -12,12 +10,7 @@ function App() {
     padding: '1rem'
   }
   return (
-      <Wrapper>
-      {/*주석 사용 방법*/}
-        <Hello isSpecial/>
-        <div style={style}>{name}</div>
-        <div className='gray-box'></div>
-      </Wrapper>
+    <Counter></Counter>
   );
 }
 
