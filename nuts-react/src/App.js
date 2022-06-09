65f1ed8e96845a22944e4ea8b531fd8514a32713
hsh0367 <hsh0367@gmail.com>
2022-06-10 02:19:26 +0900

    feat: ch6 공부 추가 밸로퍼트 react 사이트

--- nuts-react/src/App.js
+++ nuts-react/src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import Hello from './hello';
-
+import './App.css';
+import Wrapper from './Wrapper';
+import Counter from './Counter';
 function App() {
+  const name = "first";
+  const style = {
+    backgroundColor: "black",//주석 사용 방법
+    color: "aqua",
+    fontSize: 20,
+    padding: '1rem'
+  }
   return (
-      <div>
-        <Hello/>
-      </div>
+      <Wrapper>
+      {/*주석 사용 방법*/}
+        <Hello isSpecial/>
+        <div style={style}>{name}</div>
+        <div className='gray-box'></div>
+      </Wrapper>
   );
 }
 
--- nuts-react/src/hello.js
+++ nuts-react/src/hello.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
-function Hello(){
-    return <div>안녕 안녕</div>
+function Hello(props){
+    return <div style={{color: props.color}}>{props.isSpecial && <b>*</b>}안녕 안녕 {props.name}</div>
 }
 
+Hello.defaultProps = {
+    name : "이름을 설정하지 않았습니다.",
+    color: "black"
+}
 export default Hello;
