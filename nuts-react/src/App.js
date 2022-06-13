5b1a428b35922c7b993fdc46302598028b15e189
hsh0367 <hsh0367@gmail.com>
2022-06-14 02:34:25 +0900

    feat: 1-ch14 공부 추가 밸로퍼트 react 사이트

--- nuts-react/src/App.js
+++ nuts-react/src/App.js
@@ -38,6 +38,27 @@ function App() {
     });
     nextId.current += 1;
   }
+
+  /*
+  질문 ------
+  onClick={() => onRemove(user.id)} 이 코드는 잘 동작하는데
+  onClick={onRemove(user.id)} 이 코드는 왜 동작하지 않는건가요?
+
+  답변 ---
+  onClick={someFunction()} 을 해버리면 해당 콤포넌트가 렌더링이 되는것과 동시에 someFunction함수를 실행시켜버립니다.
+
+  그래서 보통 onClick={someFunction} 으로 지정해서 () 를 제외하는 방법으로 함수가 즉시실행 되지 않게 하고, 클릭했을때 실행이 되도록 해주죠
+
+  그런데 예제와 같이 onRemove의 경우, 해당 함수가 실행될 떄 아이디 값도 받아와야 하잖아요.
+  이런 경우에 onClick = { onRemove(user.id) } 를 해버리면, 해당 콤포넌트가 렌더링됨가 동시에 이 함수 실행이 되어버려서 아마 아무것도 렌더링이 되어버리지 않을거에요. 콘솔에서도 오류메시지가 발생할거구요.
+
+  따라서 이런 문제들을 해결하기 위해 onClick에 콜백 함수를 넣어주고, 해당 함수가 실행될 때 user.id를 건네주어 실행시키는 방법으로 처리를 하는거에요
+  */
+  const onRemove =  id =>{
+    // user.id 가 파라미터로 일치하지 않는 원소만 추출해서 새로운 배열을 만듬
+    // = user.id 가 id인것을 제거함
+    setUsers(users.filter(user => user.id !== id));
+  };
   return (
     <>
       <CreateUser
@@ -45,7 +66,7 @@ function App() {
         email={email}
         onChange={onChange}
         onCreate={onCreate}/>
-        <UserList users={users}/>
+        <UserList users={users} onRemove={onRemove}/>
 
     </>
   );
--- nuts-react/src/UserList.js
+++ nuts-react/src/UserList.js
@@ -5,20 +5,21 @@ Map을 이용한 렌더링
 Map에서 Key가 필요한 이유
     Map에 key 값이없다면 중간의 값이 바뀌었을때 그 하위 값들이 전부 변하기 때문인다. key값을 사용한다면 key를 이용해 중간의 값을 추가하게 된다.
 */
- function User({user}){
+ function User({user, onRemove}){
     return (
         <div> 
             <b>{user.username}</b> <span>({user.email})</span>
+            <button onClick={()=> onRemove(user.id)}> 삭제</button>
         </div>
     )
  }
 
- function UserList({users}){
+ function UserList({users, onRemove}){
     return (
             <div>
                 {
                     users.map((user, index) => (
-                        <User user={user} key={index}/>
+                        <User user={user} key={index} onRemove={onRemove}/>
                     ))
                 }
             </div>
