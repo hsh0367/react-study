08595052d46850129d15f43f452061f1a10b78e8
hsh0367 <hsh0367@gmail.com>
2022-06-15 02:50:59 +0900

    feat: 1-ch16 공부 추가 밸로퍼트 react 사이트

--- nuts-react/src/UserList.js
+++ nuts-react/src/UserList.js
@@ -1,11 +1,21 @@
- import React from "react";
+ import React, {useEffect} from "react";
 /*
-Map을 이용한 렌더링
-    arr.map(i => ) 의 형태로 하위 컴포넌트에게 값을 전달해준다.
-Map에서 Key가 필요한 이유
-    Map에 key 값이없다면 중간의 값이 바뀌었을때 그 하위 값들이 전부 변하기 때문인다. key값을 사용한다면 key를 이용해 중간의 값을 추가하게 된다.
+useEffect
+    화면이 처음 떴을때 실행.
+    deps에 [] 빈배열을 넣을 떄.
+    life cycle중 componentDidmount처럼 실행
+    화면이 사라질때 실행(clean up함수).
+    componentWillUnmount처럼 실행
+    deps에 넣은 파라미터값이 업데이트 됬을때 실행.
+    componentDidUpdate처럼 실행.
 */
  function User({user, onRemove, onToggle}){
+    useEffect(()=>{
+        console.log('user 값이 설정됨');
+        return () => {
+            console.log('user 가 바뀌기 전..');
+        };
+    }, [user])
     return (
         <div> 
             <b style={{
