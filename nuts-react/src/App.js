d193a269e7cf78c208d3873881a71624d94aa9dd
hsh0367 <hsh0367@gmail.com>
2022-06-16 02:00:53 +0900

    feat: 1-ch17 공부 추가 밸로퍼트 react 사이트

--- nuts-react/src/App.js
+++ nuts-react/src/App.js
@@ -1,8 +1,17 @@
-import React, {useRef, useState} from 'react';
+import React, {useRef, useState, useMemo} from 'react';
 import './App.css';
 import UserList from './UserList';
 import CreateUser from './CreateUser';
+
+// function countActiveUsers(users){
+//   console.log('활성 사용자 수를 세는중...')
+//   return users.filter(user => user.active).length;
+// }
 function App() {
+  const countActiveUsers = () => {
+    console.log('활성 사용자 수를 세는중...')
+    return users.filter(user => user.active).length;
+  }
   const [inputs, setInputs] = useState({
     username: '',
     email: '',
@@ -70,6 +79,7 @@ function App() {
       )
     );
   };
+  const count = useMemo( () => countActiveUsers(users), [users]);
   return (
     <>
       <CreateUser
@@ -77,8 +87,8 @@ function App() {
         email={email}
         onChange={onChange}
         onCreate={onCreate}/>
-        <UserList users={users} onRemove={onRemove} onToggle={onToggle}/>
-
+      <UserList users={users} onRemove={onRemove} onToggle={onToggle}/>
+      <div> 활성사용자 수 : {count}</div>
     </>
   );
 }
