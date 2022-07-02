a715ff528c3dd698eaf9bdc3cb30d57dbb953fff
hsh0367 <hsh0367@gmail.com>
2022-07-02 17:11:32 +0900

    feat: 1-ch26 componentDidCatch  벨로퍼트 react 사이트

--- nuts-react/src/hello.js
+++ nuts-react/src/hello.js
@@ -4,8 +4,16 @@ class Hello extends Component {
     render(){
         const {color, name, isSpecial} = this.props;
         return(
-            {isSpecial && <b>*</b>}
-            안녕하세요 {name}
-        )
+            <div>
+               {isSpecial && <b>*</b>}
+                안녕하세요 {name}
+            </div>
+        );
     }
 }
+
+Hello.defaultProps = {
+    name: '이름없음'
+};
+
+export default Hello;
