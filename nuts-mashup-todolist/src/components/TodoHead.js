492f28184a5fd6d7c9c2f5791b7a5e32fe1fbb07
hsh0367 <hsh0367@gmail.com>
2022-07-20 03:40:41 +0900

    feat: 3ch-2 밸로퍼트 react 사이트 공부 정리

--- nuts-mashup-todolist/src/components/TodoHead.js
+++ nuts-mashup-todolist/src/components/TodoHead.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-
+import { useTodoState } from './TodoContext';
 const TodoHeadBlock = styled.div`
     padding-top: 48px;
     padding-left: 32px;
@@ -26,14 +26,17 @@ const TodoHeadBlock = styled.div`
     }
 `;
 
-function TodoHead(){
+function TodoHead() {
+    const todos = useTodoState();
+    console.log(todos);
     return (
-        <TodoHeadBlock>
-            <h1>2022-07-14</h1>
-            <div className='day'>수요일</div>
-            <div className='tasks-left'>할 일 2개 남음</div>
-        </TodoHeadBlock>
+      <TodoHeadBlock>
+        <h1>2019년 7월 10일</h1>
+        <div className="day">수요일</div>
+        <div className="tasks-left">할 일 2개 남음</div>
+      </TodoHeadBlock>
     );
-}
+  }
+  
 export default TodoHead;
 
