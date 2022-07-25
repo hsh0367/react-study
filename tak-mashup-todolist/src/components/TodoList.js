75b156fc747e4894e689d5716beef92766ab72ed
takgung6820 <takgung6820@gmail.com>
2022-07-25 19:15:14 +0900

    기능 구현

--- tak-mashup-todolist/src/components/TodoList.js
+++ tak-mashup-todolist/src/components/TodoList.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import TodoItem from "./TodoItem";
+import { useTodoState } from "../TodoContext";
 
 const TodoListBlock = styled.div`
   flex: 1;
@@ -10,12 +11,18 @@ const TodoListBlock = styled.div`
 `;
 
 function TodoList() {
+  const todos = useTodoState();
+
   return (
     <TodoListBlock>
-      <TodoItem text="빨래 찾아오기" done={true} />
-      <TodoItem text="React 공부하기" done={true} />
-      <TodoItem text="서울둘레길 완주하기" done={false} />
-      <TodoItem text="5km 이상 러닝하기" done={false} />
+      {todos.map(todo => (
+        <TodoItem
+          key = {todo.id}
+          id = {todo.id}
+          text = {todo.text}
+          done = {todo.done}
+        />
+      ))}
     </TodoListBlock>
   );
 }
