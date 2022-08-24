4f8d658f0fbb9a214707a6f4a9f6c52226aa9f67
takgung6820 <takgung6820@gmail.com>
2022-08-24 19:32:34 +0900

    리액트 라우터 부가기능

--- tak-router-tutorial/src/Profiles.js
+++ tak-router-tutorial/src/Profiles.js
@@ -1,6 +1,7 @@
-import React from "react";
-import { Link, Route } from 'react-router-dom';
-import Profile from "./Profile";
+import React from 'react';
+import {Route, NavLink } from 'react-router-dom';
+import Profile from './Profile';
+import WithRouterSample from './WithRouterSample';
 
 const Profiles = () => {
   return (
@@ -8,19 +9,30 @@ const Profiles = () => {
       <h3>유저 목록:</h3>
       <ul>
         <li>
-          <Link to="/profiles/wontak">wontak</Link>
+          <NavLink
+            to="/profiles/velopert"
+            activeStyle={{ background: 'black', color: 'white' }}
+          >
+            velopert
+          </NavLink>
         </li>
         <li>
-          <Link to="/profiles/sanghoon">sanghoon</Link>
+          <NavLink
+            to="/profiles/gildong"
+            activeStyle={{ background: 'black', color: 'white' }}
+          >
+            gildong
+          </NavLink>
         </li>
       </ul>
 
       <Route
         path="/profiles"
         exact
-        render={() => <div>유저를 선택해 주세요.</div>}
+        render={() => <div>유저를 선택해주세요.</div>}
       />
       <Route path="/profiles/:username" component={Profile} />
+      <WithRouterSample />
     </div>
   );
 };
