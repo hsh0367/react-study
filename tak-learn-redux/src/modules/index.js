376b810f2e5662ff0784ef76a2cedae83efbc02a
takgung6820 <takgung6820@gmail.com>
2022-09-14 19:44:36 +0900

    할 일 목록 구현

--- tak-learn-redux/src/modules/index.js
+++ tak-learn-redux/src/modules/index.js
@@ -1,22 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-import rootReducer from './modules';
-import { composeWithDevTools } from 'redux-devtools-extension'; // 리덕스 개발자 도구
+import { combineReducers } from "redux";
+import counter from "./counter";
+import todos from "./todos";
 
-const store = createStore(rootReducer, composeWithDevTools()); // 스토어를 만듭니다.
-// composeWithDevTools 를 사용하여 리덕스 개발자 도구 활성화
+const rootReducer = combineReducers({
+  counter,
+  todos
+});
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-);
-
-
-serviceWorker.unregister();
+export default rootReducer;
