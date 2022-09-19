7e963c43663005f9465cebf9c5217ea4911dcd0c
takgung6820 <takgung6820@gmail.com>
2022-09-19 18:21:32 +0900

    connect 함수 사용

--- tak-learn-redux/src/containers/CounterContainer.js
+++ tak-learn-redux/src/containers/CounterContainer.js
@@ -1,34 +1,44 @@
-import React from "react";
-import { useSelector, useDispatch } from "react-redux";
-import Counter from "../components/Counter";
-import { increase, decrease, setDiff } from "../modules/counter";
+import React from 'react';
+import { connect } from 'react-redux';
+import Counter from '../components/Counter';
+import { increase, decrease, setDiff } from '../modules/counter';
 
-function CounterContainer() {
-  // useSelector는 리덕스 스토어의 상태를 조회하는 Hook입니다.
-  // state의 값은 store.getState() 함수를 호출했을 때 나타나는 결과물과 동일합니다.
-  const { number, diff } = useSelector(state => ({
-    number: state.counter.number,
-    diff: state.counter.diff
-  }));
-
-  // useDispatch는 리덕스 스토어의 dispatch를 함수에서 사용할 수 있게 해주는 Hook입니다.
-  const dispatch = useDispatch();
-  // 각 액션들을 dispatch하는 함수들을 만드세요
-  const onIncrease = () => dispatch(increase());
-  const onDecrease = () => dispatch(decrease());
-  const onSetDiff = () => dispatch(setDiff(diff));
-
+function CounterContainer({ number, diff, increase, decrease, setDiff }) {
   return (
     <Counter
       // 상태와
       number={number}
       diff={diff}
-      // 액션을 dispatch하는 함수들을 props로 넣어줍니다.
-      onIncrease={onIncrease}
-      onDecrease={onDecrease}
-      onSetDiff={onSetDiff}
+      // 액션을 디스패치 하는 함수들을 props로 넣어줍니다.
+      onIncrease={increase}
+      onDecrease={decrease}
+      onSetDiff={setDiff}
     />
   );
 }
 
-export default CounterContainer;
+// mapStateToProps 는 리덕스 스토어의 상태를 조회해서 어떤 것들을 props 로 넣어줄지 정의합니다.
+// 현재 리덕스 상태를 파라미터로 받아옵니다.
+const mapStateToProps = state => ({
+  number: state.counter.number,
+  diff: state.counter.diff
+});
+
+// mapDispatchToProps가 함수가 아니라 객체면
+// bindActionCreators 를 connect 에서 대신 해줍니다.
+const mapDispatchToProps = {
+  increase,
+  decrease,
+  setDiff
+};
+
+// connect 함수에는 mapStateToProps, mapDispatchToProps 를 인자로 넣어주세요.
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(CounterContainer);
+
+/* 위 코드는 다음과 동일합니다.
+  const enhance = connect(mapStateToProps, mapDispatchToProps);
+  export defualt enhance(CounterContainer);
+*/
