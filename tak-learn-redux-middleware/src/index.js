1e3921d3581f5ae0dfc959311a47caa42985957a
takgung6820 <takgung6820@gmail.com>
2022-09-25 16:36:35 +0900

    미들웨어 만들어보기

--- tak-learn-redux-middleware/src/index.js
+++ tak-learn-redux-middleware/src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer from './modules';
+import myLogger from './middlewares/myLogger';
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(myLogger));
 
 ReactDOM.render(
   <Provider store={store}>
@@ -16,7 +17,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
